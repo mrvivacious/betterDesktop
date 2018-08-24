9cb71ac961fbac79e297e474308bbd3710bd1674
Vivek Bhookya <v.bhookya98@gmail.com>
2018-08-24 10:05:54 -0500

    subject based layout is easier to read than the HW-NOTES exclusive layout

--- javascript.js
+++ javascript.js
@@ -4,24 +4,54 @@
 // TODO::Set up a handy dandy CMD+S (autosave feature :O ???)
 
 init();
-
 // Function init()
 // Reloads data from localStorage into the textareas
 function init() {
   let hw = localStorage.getItem('hw');
   let notes = localStorage.getItem('notes');
 
-  document.getElementById('hw').value = hw;
-  document.getElementById('notes').innerHTML = notes;
+  let v_241 = localStorage.getItem('d_241');
+  let v_200 = localStorage.getItem('d_200');
+  let v_330 = localStorage.getItem('d_330');
+  let v_350 = localStorage.getItem('d_350');
+  let v_402 = localStorage.getItem('d_402');
+  let v_201 = localStorage.getItem('d_201');
+  let v_extrac = localStorage.getItem('d_extrac');
+  let v_other = localStorage.getItem('d_other');
+
+  // document.getElementById('hw').value = hw;
+  // document.getElementById('notes').innerHTML = notes;
+
+  document.getElementById('241').value = v_241;
+  document.getElementById('200').value = v_200;
+  document.getElementById('330').value = v_330;
+  document.getElementById('350').value = v_350;
+  document.getElementById('402').value = v_402;
+  document.getElementById('201').value = v_201;
+  document.getElementById('extrac').value = v_extrac;
+  document.getElementById('other').value = v_other;
 }
 
 // Function save()
 // Writes written data into localStorage when the save button
 //  is clicked
 function save() {
-  let hw = document.getElementById('hw').value;
-  let notes = document.getElementById('notes').value;
+  // d_ = data
+  let d_241 = document.getElementById('241').value;
+  let d_200 = document.getElementById('200').value;
+  let d_330 = document.getElementById('330').value;
+  let d_350 = document.getElementById('350').value;
+  let d_402 = document.getElementById('402').value;
+  let d_201 = document.getElementById('201').value;
+  let d_extrac = document.getElementById('extrac').value;
+  let d_other = document.getElementById('other').value;
 
-  localStorage.setItem('hw', hw);
-  localStorage.setItem('notes', notes);
+  localStorage.setItem('d_241', d_241);
+  localStorage.setItem('d_200', d_200);
+  localStorage.setItem('d_330', d_330);
+  localStorage.setItem('d_350', d_350);
+  localStorage.setItem('d_402', d_402);
+  localStorage.setItem('d_201', d_201);
+  localStorage.setItem('d_extrac', d_extrac);
+  localStorage.setItem('d_other', d_other);
 }
